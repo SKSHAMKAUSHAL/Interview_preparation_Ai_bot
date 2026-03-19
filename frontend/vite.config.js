1ced5cdfdfbabf87a8b1031c74c376f4508737eb
Sksham Kaushal <skshamkaushal@gmail.com>
2026-03-19 23:20:37 +0530

    Fixing URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  },
+  server: {
+    strictPort: false,
+    host: '0.0.0.0'
+  }
 })
